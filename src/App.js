c3316ed84923b434e46744d08a87b279e005e8d4
Anthony DiPadova <tdipadova3rd@gmail.com>
2022-06-10 17:45:30 -0400

    Add tailwind css

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       </header>
     </div>
   );
